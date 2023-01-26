6bd29db9761aacaba4b49a54376e4b963e45eb25
MO JHU <mowen4@jh.edu>
2023-01-26 16:18:34 -0500

    alpha release with comments

--- src/TriangleTest.cpp
+++ src/TriangleTest.cpp
@@ -16,15 +16,42 @@ class InputOutput
     public:
         void getInputs(int inputs[3])
         {
+            //temporary local variables
+            int temp = 0;
+            bool input_ok;
 
             std::cout << "Triangle Tester" << std::endl;
-            std::cout << "Enter 1st Side Length: ";
-            std::cin >> inputs[0];
-            std::cout << "Enter 2nd Side Length: ";
-            std::cin >> inputs[1];
-            std::cout << "Enter 3rd Side Length: ";
-            std::cin >> inputs[2];
+            
+            //for each side repeat the inner loop until a valid integer input is detected
+            for ( int i = 0; i < 3; ++i )
+            {
+                do
+                {
+                    cout << "Enter side " << i +1 <<":";
+                    cin >> inputs[i];
 
+                    if ( cin.fail() )
+                    {
+                        cout << "Error: Conversion to integer failed!\n";
+
+                        input_ok = false;
+                    }
+                    else if ( inputs[i] > 2147483647 || inputs[i] <= 0 )
+                    {
+                        cout << "Error: Number should in range of (1 to 2147483647) only!\n";
+                        input_ok = false;
+                    }
+                    else
+                    {
+                        input_ok = true;
+                    }
+
+                    cin.clear();
+
+                    cin.ignore(2147483647, '\n' );
+                } while ( !input_ok );
+            
+            }
         }
 };
 
@@ -45,30 +72,35 @@ class Triangle
         void determineTriangleType()
         {
 
-            std::cout << a << b << c << std::endl;
+            //std::cout << a << b << c << std::endl;
             
             bool noType = true;
             
+            //check if trianle is valid size
             if(!isValidTriangle())
             {
                 std::cout << "You have entered invalid lengths for the sides of a real triangle." << endl;
                 noType = false;
             }
-            if (a == b && b == c && a == c)
+            //if a==b and b==c then a==b==c
+            if (a == b && b == c)
             {
                 std::cout << "It is Equilateral" << std::endl;
                 noType = false;
             }
+            //if any pair of sides is equal then it is also Isosceles
             if (a == b || a == c || b == c)
             {
                 std::cout << "It is Isosceles" << std::endl;
                 noType = false;
             }
-            if ((a*a + b*b == c*c) || (b*b + c*c == a*a))
+            //if any combination of legs meet pythagorean theorem it is a right triangle
+            if ((a*a + b*b == c*c) || (b*b + c*c == a*a) || (c*c + a*a == b*b))
             {
                 std::cout << "It is a Right Triangle" << std::endl;
                 noType = false;
             }
+            //if none are met then it is a nonstandard type
             if (noType)
             {
                 std::cout << "It is none of the standard types" << std::endl;
