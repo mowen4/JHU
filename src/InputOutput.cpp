e5efb8718647ed651747be3a2486ff73375ab600
MO JHU <mowen4@jh.edu>
2023-01-28 18:10:10 -0500

    formatting and comments

--- src/InputOutput.cpp
+++ src/InputOutput.cpp
@@ -6,47 +6,46 @@ using namespace std;
 
 class InputOutput
 {
-    //TODO: Checking for errors, for negative values and potentially too large values?
 
-    public:
-        void getInputs(int inputs[3])
+public:
+    void getInputs(int inputs[3])
+    {
+        // temporary local variable
+
+        std::cout << "Triangle Tester" << std::endl;
+
+        // for each side repeat the inner loop until a valid integer input is detected
+        for (int i = 0; i < 3; ++i)
         {
-            //temporary local variable
-
-            std::cout << "Triangle Tester" << std::endl;
-            
-            //for each side repeat the inner loop until a valid integer input is detected
-            for ( int i = 0; i < 3; ++i )
+            do
             {
-                do
+                cout << "Enter side " << i + 1 << ":";
+                cin >> inputs[i];
+
+                if (cin.fail())
                 {
-                    cout << "Enter side " << i +1 <<":";
-                    cin >> inputs[i];
+                    cout << "Error: Conversion to integer failed!\n";
 
-                    if ( cin.fail() )
-                    {
-                        cout << "Error: Conversion to integer failed!\n";
+                    input_ok = false;
+                }
+                else if (inputs[i] > 2147483647 || inputs[i] <= 0)
+                {
+                    cout << "Error: Number should in range of (1 to 2147483647) only!\n";
+                    input_ok = false;
+                }
+                else
+                {
+                    input_ok = true;
+                }
 
-                        input_ok = false;
-                    }
-                    else if ( inputs[i] > 2147483647 || inputs[i] <= 0 )
-                    {
-                        cout << "Error: Number should in range of (1 to 2147483647) only!\n";
-                        input_ok = false;
-                    }
-                    else
-                    {
-                        input_ok = true;
-                    }
+                cin.clear();
 
-                    cin.clear();
-
-                    cin.ignore(2147483647, '\n' );
-                } while ( !input_ok );            
-            }
+                cin.ignore(2147483647, '\n');
+            } while (!input_ok);
         }
-    
-    private:
-            int temp = 0;
-            bool input_ok;
+    }
+
+private:
+    int temp = 0;
+    bool input_ok;
 };
--- src/TriangleTest.cpp
+++ src/TriangleTest.cpp
@@ -1,27 +1,26 @@
+//Module 1 Assignment Michael Owen David Wittsack
+
 #include "InputOutput.cpp"
 #include "Triangle.cpp"
 
 using namespace std;
 
-//program entry point
+// program entry point
 int main()
 {
-    //array to hold the user input values
-    int sides[3] = {0,0,0};
+    // array to hold the user input values
+    int sides[3] = {0, 0, 0};
 
     InputOutput io;
-    //gets user input and saves into sides array
+    // gets user input and saves into sides array
     io.getInputs(sides);
 
-    //creates triangle object using sides array
-    Triangle t(sides[0],sides[1],sides[2]);
+    // creates triangle object using sides array
+    Triangle t(sides[0], sides[1], sides[2]);
 
-    //check the triangle type and output it to the console
+    // check the triangle type and output it to the console
     t.determineTriangleType();
 
-    //return zero for successful completion
+    // return zero for successful completion
     return 0;
 }
-
-
-
