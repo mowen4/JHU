8f75ff9d1edf02ccb21afc03396c0dd3327156f7
dwittty <66267250+dwittty@users.noreply.github.com>
2023-01-28 14:03:09 -0500

    added some more code comments

--- src/TriangleTest.cpp
+++ src/TriangleTest.cpp
@@ -1,6 +1,5 @@
-//hello world for the git repo
+
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <stdlib.h>
@@ -10,16 +9,23 @@
 
 using namespace std;
 
+//program entry point
 int main()
 {
+    //array to hold the user input values
     int sides[3] = {0,0,0};
 
     InputOutput io;
+    //gets user input and saves into sides array
     io.getInputs(sides);
 
+    //creates triangle object using sides array
     Triangle t(sides[0],sides[1],sides[2]);
+
+    //check the triangle type and output it to the console
     t.determineTriangleType();
 
+    //return zero for successful completion
     return 0;
 }
 
