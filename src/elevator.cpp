6acbc62c7cebe4923578c185af400ffeb6fc83d9
MO JHU <mowen4@jh.edu>
2023-04-10 17:11:25 -0400

    1st pass

---
+++ src/elevator.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+const int MAX_ELEVATORS = 4;
+const int MAX_PASSENGERS = 8;
+const int MAX_FLOORS = 100;
+
+enum ElevatorState {
+    STOPPED,
+    STOPPING,
+    MOVING_UP,
+    MOVING_DOWN
+};
+
+class Passenger {
+public:
+    Passenger(int start_time, int start_floor, int end_floor) :
+        start_time(start_time), start_floor(start_floor), end_floor(end_floor) {}
+
+    int get_start_time() const { return start_time; }
+    int get_start_floor() const { return start_floor; }
+    int get_end_floor() const { return end_floor; }
+
+private:
+    int start_time;
+    int start_floor;
+    int end_floor;
+};
+
+class Elevator {
+public:
+    Elevator() : state(STOPPED), current_floor(1), num_passengers(0) {}
+
+    void add_passenger(Passenger passenger) {
+        if (num_passengers < MAX_PASSENGERS) {
+            passengers.push_back(passenger);
+            num_passengers++;
+        }
+    }
+
+    void move_up() {
+        state = MOVING_UP;
+        current_floor++;
+        for (auto it = passengers.begin(); it != passengers.end(); ) {
+            if (it->get_end_floor() == current_floor) {
+                it = passengers.erase(it);
+                num_passengers--;
+            } else {
+                ++it;
+            }
+        }
+    }
+
+    void move_down() {
+        state = MOVING_DOWN;
+        current_floor--;
+        for (auto it = passengers.begin(); it != passengers.end(); ) {
+            if (it->get_end_floor() == current_floor) {
+                it = passengers.erase(it);
+                num_passengers--;
+            } else {
+                ++it;
+            }
+        }
+    }
+
+    void stop() {
+        state = STOPPING;
+        stop_time_left = 2;
+    }
+
+    void update() {
+        if (state == STOPPING) {
+            stop_time_left--;
+            if (stop_time_left == 0) {
+                state = STOPPED;
+            }
+        } else if (state == MOVING_UP) {
+            if (current_floor == MAX_FLOORS) {
+                stop();
+            } else {
+                move_up();
+            }
+        } else if (state == MOVING_DOWN) {
+            if (current_floor == 1) {
+                stop();
+            } else {
+                move_down();
+            }
+        }
+    }
+
+    ElevatorState get_state() const { return state; }
+    int get_current_floor() const { return current_floor; }
+    int get_num_passengers() const { return num_passengers; }
+
+private:
+    ElevatorState state;
+    int current_floor;
+    int stop_time_left;
+    vector<Passenger> passengers;
+    int num_passengers;
+};
+
+class Floors {
+public:
+    Floors() {
+        for (int i = 0; i < MAX_ELEVATORS; i++) {
+            elevators.push_back(Elevator());
+        }
+    }
+
+    void add_passenger(Passenger passenger) {
+        passengers.push_back(passenger);
+    }
+
+    void update() {
+        for (auto& elevator : elevators) {
+            if (elevator.get_state() == STOPPED) {
+                int min_dist = MAX_FLOORS;
+                int closest_passenger_index = -1;
+                for (int i = 0; i < passengers.size(); i++) {
+                    Passenger& passenger = passengers[i];
+                    if (passenger.get_start_floor() == elevator.get_current_floor()) {
+                        if (passenger.get_start_time() <= current_time) {
+                            int dist = abs(elevator.get_current_floor() - passenger.get_start_floor());
+                            if (dist < min_dist) {
+                                min_dist = dist;
+                                closest_passenger_index = i;
+                            }
+                        }
+                    }
+                }
+                if (closest_passenger_index != -1) {
+                    elevator.add_passenger(passengers[closest_passenger_index]);
+                    passengers.erase(passengers.begin() + closest_passenger_index);
+                    elevator.stop();
+                }
+            }
+            elevator.update();
+        }
+        current_time++;
+    }
+
+    int get_current_time() const { return current_time; }
+
+private:
+    vector<Elevator> elevators;
+    vector<Passenger> passengers;
+    int current_time = 0;
+};
+
+
+
+void getdata() {
+    string fname = "Mod10_Assignment_Elevators.csv";
+ 
+	vector<vector<string>> content;
+	vector<string> row;
+	string line, word;
+ 
+	fstream file (fname, ios::in);
+	if(file.is_open())
+	{
+		while(getline(file, line))
+		{
+			row.clear();
+ 
+			stringstream str(line);
+ 
+			while(getline(str, word, ','))
+				row.push_back(word);
+			content.push_back(row);
+		}
+	}
+	else
+		cout<<"Could not open the file\n";
+ 
+	for(vector v : content)
+	{
+		for(auto & element: v )
+		{
+			cout<<element<<" ";
+		}
+		cout<<"\n";
+	}
+}
+
+int main() {
+    Floors f;
+    // TODO: read passengers from Elevator.csv and add them to building
+    getdata();
+
+    // while (true) {
+    //     f.update();
+    //     // TODO: check if all passengers have reached their destination and exit loop if so
+    // }
+
+    // TODO: calculate and print average wait time and average travel time
+    return 0;
+}
